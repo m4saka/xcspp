89f992ac51b7b6cddc030e22339307709941193b
masaka <masaka@mz-kb.com>
2020-08-27 18:05:39 +0900

    Add classifier class

---
+++ include/xcscpp/classifier.hpp
@@ -0,0 +1,242 @@
+#pragma once
+#include <string>
+#include <unordered_set>
+#include <memory>
+#include <cstdint> // std::uint64_t
+#include <cstddef> // std::size_t
+#include <cassert>
+
+#include "condition.hpp"
+#include "constants.hpp"
+
+namespace xcscpp
+{
+
+    struct ConditionActionPair
+    {
+    public:
+        // C
+        //   The condition specifies the input states (sensory situations)
+        //   in which the classifier can be applied (matches).
+        Condition condition;
+
+        // A
+        //   The action specifies the action (possibly a classification)
+        //   that the classifier proposes.
+        int action;
+
+        // Constructor
+        ConditionActionPair(const ConditionActionPair &) = default;
+
+        ConditionActionPair(const Condition & condition, int action) : condition(condition), action(action) {}
+
+        ConditionActionPair(Condition && condition, int action) : condition(std::move(condition)), action(action) {}
+
+        // Destructor
+        virtual ~ConditionActionPair() = default;
+
+        // IS MORE GENERAL
+        bool isMoreGeneral(const ConditionActionPair & cl) const
+        {
+            if (condition.size() != cl.condition.size())
+            {
+                std::invalid_argument("ConditionActionPair::isMoreGeneral() could not process the classifier with a different condition length.");
+            }
+
+            bool ret = false;
+
+            for (std::size_t i = 0; i < condition.size(); ++i)
+            {
+                if (condition.at(i) != cl.condition.at(i))
+                {
+                    if (!condition.at(i).isDontCare())
+                    {
+                        return false;
+                    }
+                    else 
+                    {
+                        ret = true;
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        friend std::ostream & operator<< (std::ostream & os, const ConditionActionPair & obj)
+        {
+            return os << obj.condition << ':' << obj.action;
+        }
+    };
+
+    struct Classifier : ConditionActionPair
+    {
+    public:
+        // p
+        //   The prediction p estimates (keeps an average of) the payoff expected if the
+        //   classifier matches and its action is taken by the system.
+        double prediction;
+
+        // epsilon
+        //   The prediction error epsilon estimates the errors made in the predictions.
+        double epsilon;
+
+        // F
+        //   The fitness F denotes the classifier's fitness.
+        double fitness;
+
+        // exp
+        //   The experience exp counts the number of times since its creation that the
+        //   classifier has belonged to an action set.
+        std::uint64_t experience;
+
+        // ts
+        //   The time stamp ts denotes the time-step of the last occurrence of a GA in
+        //   an action set to which this classifier belonged.
+        std::uint64_t timeStamp;
+
+        // as
+        //   The action set size as estimates the average size of the action sets this
+        //   classifier has belonged to.
+        double actionSetSize;
+
+        // n
+        //   The numerosity n reflects the number of micro-classifiers (ordinary
+        //   classifiers) this classifier - which is technically called a macro-
+        //   classifier - represents.
+        std::uint64_t numerosity;
+
+        // Constructor
+        Classifier(const Classifier &) = default;
+
+        Classifier(const Condition & condition, int action, double prediction, double epsilon, double fitness, std::uint64_t timeStamp)
+            : ConditionActionPair(condition, action)
+            , prediction(prediction)
+            , epsilon(epsilon)
+            , fitness(fitness)
+            , experience(0)
+            , timeStamp(timeStamp)
+            , actionSetSize(1)
+            , numerosity(1)
+        {
+        }
+
+        Classifier(const ConditionActionPair & conditionActionPair, double prediction, double epsilon, double fitness, std::uint64_t timeStamp)
+            : ConditionActionPair(conditionActionPair)
+            , prediction(prediction)
+            , epsilon(epsilon)
+            , fitness(fitness)
+            , experience(0)
+            , timeStamp(timeStamp)
+            , actionSetSize(1)
+            , numerosity(1)
+        {
+        }
+
+        Classifier(ConditionActionPair && conditionActionPair, double prediction, double epsilon, double fitness, std::uint64_t timeStamp)
+            : ConditionActionPair(std::move(conditionActionPair))
+            , prediction(prediction)
+            , epsilon(epsilon)
+            , fitness(fitness)
+            , experience(0)
+            , timeStamp(timeStamp)
+            , actionSetSize(1)
+            , numerosity(1)
+        {
+        }
+
+        Classifier(const std::vector<int> & situation, int action, double prediction, double epsilon, double fitness, std::uint64_t timeStamp)
+            : Classifier(Condition(situation), action, prediction, epsilon, fitness, timeStamp)
+        {
+        }
+
+        Classifier(const std::string & condition, int action, double prediction, double epsilon, double fitness, std::uint64_t timeStamp)
+            : Classifier(Condition(condition), action, prediction, epsilon, fitness, timeStamp)
+        {
+        }
+
+        // Destructor
+        virtual ~Classifier() = default;
+
+        double accuracy(double epsilonZero, double alpha, double nu) const
+        {
+            if (epsilon < epsilonZero)
+            {
+                return 1.0;
+            }
+            else
+            {
+                return alpha * pow(epsilon / epsilonZero, -nu);
+            }
+        }
+    };
+
+    // Classifier in [P] (have a reference to Constants)
+    struct StoredClassifier : Classifier
+    {
+    protected:
+        // Constants
+        Constants & m_constants;
+
+    public:
+        // Constructor
+        StoredClassifier(const StoredClassifier & obj) = default;
+
+        StoredClassifier(const Classifier & obj, Constants & constants)
+            : Classifier(obj)
+            , m_constants(constants)
+        {
+        }
+
+        StoredClassifier(const Condition & condition, int action, std::uint64_t timeStamp, Constants & constants)
+            : Classifier(condition, action, constants.initialPrediction, constants.initialEpsilon, constants.initialFitness, timeStamp)
+            , m_constants(constants)
+        {
+        }
+
+        StoredClassifier(const ConditionActionPair & conditionActionPair, std::uint64_t timeStamp, Constants & constants)
+            : Classifier(conditionActionPair, constants.initialPrediction, constants.initialEpsilon, constants.initialFitness, timeStamp)
+            , m_constants(constants)
+        {
+        }
+
+        StoredClassifier(ConditionActionPair && conditionActionPair, std::uint64_t timeStamp, Constants & constants)
+            : Classifier(std::move(conditionActionPair), constants.initialPrediction, constants.initialEpsilon, constants.initialFitness, timeStamp)
+            , m_constants(constants)
+        {
+        }
+
+        StoredClassifier(const std::vector<int> & situation, int action, std::uint64_t timeStamp, Constants & constants)
+            : Classifier(situation, action, constants.initialPrediction, constants.initialEpsilon, constants.initialFitness, timeStamp)
+            , m_constants(constants)
+        {
+        }
+
+        StoredClassifier(const std::string & condition, int action, std::uint64_t timeStamp, Constants & constants)
+            : Classifier(condition, action, constants.initialPrediction, constants.initialEpsilon, constants.initialFitness, timeStamp)
+            , m_constants(constants)
+        {
+        }
+
+        // Destructor
+        virtual ~StoredClassifier() = default;
+
+        // COULD SUBSUME
+        bool isSubsumer() const noexcept
+        {
+            return experience > m_constants.thetaSub && epsilon < m_constants.epsilonZero;
+        }
+
+        // DOES SUBSUME
+        bool subsumes(const Classifier & cl) const
+        {
+            return action == cl.action && isSubsumer() && isMoreGeneral(cl);
+        }
+
+        double accuracy() const
+        {
+            return Classifier::accuracy(m_constants.epsilonZero, m_constants.alpha, m_constants.nu);
+        }
+    };
+
+}
