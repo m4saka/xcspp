37a77dbaf87a83572dbfdda902b4f972d33ac2da
masaka <masaka@mz-kb.com>
2020-09-09 00:21:29 +0900

    Separate MatchSet implementation into source file

--- include/xcspp/match_set.hpp
+++ include/xcspp/match_set.hpp
@@ -1,13 +1,9 @@
 ﻿#pragma once
-#include <memory> // std::make_shared
-#include <unordered_map>
 #include <cstdint> // std::uint64_t
-#include <sstream> // std::ostringstream
 
 #include "classifier_ptr_set.hpp"
 #include "population.hpp"
 #include "xcs_params.hpp"
-#include "random.hpp"
 
 namespace xcspp
 {
@@ -17,85 +13,21 @@ namespace xcspp
     protected:
         bool m_isCoveringPerformed;
 
-        // GENERATE COVERING CLASSIFIER
-        ClassifierPtr generateCoveringClassifier(const std::vector<int> & situation, const std::unordered_set<int> & unselectedActions, std::uint64_t timeStamp) const
-        {
-            const auto cl = std::make_shared<StoredClassifier>(situation, Random::chooseFrom(unselectedActions), timeStamp, m_pParams);
-            cl->condition.setToDontCareAtRandom(m_pParams->dontCareProbability);
-
-            return cl;
-        }
-
     public:
         // Constructor
-        using ClassifierPtrSet::ClassifierPtrSet;
+        using ClassifierPtrSet::ClassifierPtrSet; // inherits all constructors from ClassifierPtrSet
 
-        MatchSet(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp, const XCSParams *pParams, const std::unordered_set<int> & availableActions)
-            : ClassifierPtrSet(pParams, availableActions)
-            , m_isCoveringPerformed(false)
-        {
-            regenerate(population, situation, timeStamp);
-        }
+        MatchSet(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp, const XCSParams *pParams, const std::unordered_set<int> & availableActions);
 
         // Destructor
         virtual ~MatchSet() = default;
 
         // GENERATE MATCH SET
-        void regenerate(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp)
-        {
-            // Set theta_mna (the minimal number of actions) to the number of action choices if theta_mna is 0
-            auto thetaMna = (m_pParams->thetaMna == 0) ? m_availableActions.size() : m_pParams->thetaMna;
-
-            auto unselectedActions = m_availableActions;
-
-            m_set.clear();
-
-            while (m_set.empty())
-            {
-                for (const auto & cl : population)
-                {
-                    if (cl->condition.matches(situation))
-                    {
-                        m_set.insert(cl);
-                        unselectedActions.erase(cl->action);
-                    }
-                }
-
-                // Generate classifiers covering the unselected actions
-                if (m_availableActions.size() - unselectedActions.size() < thetaMna)
-                {
-                    const auto coveringClassifier = generateCoveringClassifier(situation, unselectedActions, timeStamp);
-                    if (!coveringClassifier->condition.matches(situation))
-                    {
-                        std::ostringstream oss;
-                        oss <<
-                            "The covering classifier does not contain the current situation!\n"
-                            "  - Current situation: ";
-                        for (const auto & s : situation)
-                        {
-                            oss << s << ' ';
-                        }
-                        oss << "\n  - Covering classifier: " << *coveringClassifier << '\n' << std::endl;
-                        throw std::runtime_error(oss.str());
-                    }
-                    population.insert(coveringClassifier);
-                    population.deleteExtraClassifiers();
-                    m_set.clear();
-                    m_isCoveringPerformed = true;
-                }
-                else
-                {
-                    m_isCoveringPerformed = false;
-                }
-            }
-        }
+        void regenerate(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp);
 
         // Get if covering is performed in the previous match set generation
         // (Call this function after constructor or regenerate())
-        bool isCoveringPerformed() const
-        {
-            return m_isCoveringPerformed;
-        }
+        bool isCoveringPerformed() const;
     };
 
 }
---
+++ src/match_set.cpp
@@ -0,0 +1,87 @@
+#include "xcspp/match_set.hpp"
+#include <memory> // std::make_shared
+#include <sstream> // std::ostringstream
+
+#include "xcspp/random.hpp"
+
+namespace xcspp
+{
+
+    namespace
+    {
+        // GENERATE COVERING CLASSIFIER
+        ClassifierPtr generateCoveringClassifier(const std::vector<int> & situation, const std::unordered_set<int> & unselectedActions, std::uint64_t timeStamp, const XCSParams *pParams)
+        {
+            const auto cl = std::make_shared<StoredClassifier>(situation, Random::chooseFrom(unselectedActions), timeStamp, pParams);
+            cl->condition.setToDontCareAtRandom(pParams->dontCareProbability);
+
+            return cl;
+        }
+    }
+
+    MatchSet::MatchSet(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp, const XCSParams *pParams, const std::unordered_set<int> & availableActions)
+        : ClassifierPtrSet(pParams, availableActions)
+        , m_isCoveringPerformed(false)
+    {
+        regenerate(population, situation, timeStamp);
+    }
+
+    // GENERATE MATCH SET
+    void MatchSet::regenerate(Population & population, const std::vector<int> & situation, std::uint64_t timeStamp)
+    {
+        // Set theta_mna (the minimal number of actions) to the number of action choices if theta_mna is 0
+        auto thetaMna = (m_pParams->thetaMna == 0) ? m_availableActions.size() : m_pParams->thetaMna;
+
+        auto unselectedActions = m_availableActions;
+
+        m_set.clear();
+
+        while (m_set.empty())
+        {
+            for (const auto & cl : population)
+            {
+                if (cl->condition.matches(situation))
+                {
+                    m_set.insert(cl);
+                    unselectedActions.erase(cl->action);
+                }
+            }
+
+            // Generate classifiers covering the unselected actions
+            if (m_availableActions.size() - unselectedActions.size() < thetaMna)
+            {
+                const auto coveringClassifier = generateCoveringClassifier(situation, unselectedActions, timeStamp, m_pParams);
+
+                // Make sure the generated covering classifier covers the given input
+                if (!coveringClassifier->condition.matches(situation))
+                {
+                    std::ostringstream oss;
+                    oss <<
+                        "The covering classifier does not contain the current situation!\n"
+                        "  - Current situation: ";
+                    for (const auto & s : situation)
+                    {
+                        oss << s << ' ';
+                    }
+                    oss << "\n  - Covering classifier: " << *coveringClassifier << '\n' << std::endl;
+                    throw std::runtime_error(oss.str());
+                }
+
+                population.insert(coveringClassifier);
+                population.deleteExtraClassifiers();
+                m_set.clear();
+                m_isCoveringPerformed = true;
+            }
+            else
+            {
+                m_isCoveringPerformed = false;
+            }
+        }
+    }
+
+    bool MatchSet::isCoveringPerformed() const
+    {
+        return m_isCoveringPerformed;
+    }
+
+}
