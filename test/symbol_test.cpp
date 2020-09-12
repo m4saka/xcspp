05a1a491d014c7acefbca57ef97663420ad3f517
masaka <masaka@mz-kb.com>
2020-09-12 21:33:09 +0900

    Use EXPECT_EQ/NE rather than EXPECT_TRUE/FALSE

--- test/symbol_test.cpp
+++ test/symbol_test.cpp
@@ -11,7 +11,7 @@ TEST(SymbolTest, ConstructWithNumberChar)
 
         const xcspp::Symbol number(c);
         EXPECT_FALSE(number.isDontCare());
-        EXPECT_TRUE(number.value() == i);
+        EXPECT_EQ(number.value(), i);
         EXPECT_TRUE(number.matches(i));
         EXPECT_FALSE(number.matches(i + 1));
     }
@@ -32,7 +32,7 @@ TEST(SymbolTest, ConstructWithInt)
     {
         const xcspp::Symbol number(i);
         EXPECT_FALSE(number.isDontCare());
-        EXPECT_TRUE(number.value() == i);
+        EXPECT_EQ(number.value(), i);
         EXPECT_TRUE(number.matches(i));
         EXPECT_FALSE(number.matches(i + 1));
     }
@@ -43,30 +43,30 @@ TEST(SymbolTest, CopyConstruct)
     const xcspp::Symbol one(1);
     const xcspp::Symbol oneClone(one);
     EXPECT_FALSE(oneClone.isDontCare());
-    EXPECT_TRUE(oneClone.value() == 1);
+    EXPECT_EQ(oneClone.value(), 1);
     EXPECT_TRUE(oneClone.matches(1));
     EXPECT_FALSE(oneClone.matches(0));
-    EXPECT_TRUE(one == oneClone);
+    EXPECT_EQ(one, oneClone);
 
     const xcspp::Symbol dontCare('#');
     const xcspp::Symbol dontCareClone(dontCare);
     EXPECT_TRUE(dontCareClone.isDontCare());
     EXPECT_TRUE(dontCareClone.matches(0));
     EXPECT_TRUE(dontCareClone.matches(1));
-    EXPECT_TRUE(dontCare == dontCareClone);
+    EXPECT_EQ(dontCare, dontCareClone);
 }
 
 TEST(SymbolTest, SetValue)
 {
     xcspp::Symbol symbol(1);
     EXPECT_FALSE(symbol.isDontCare());
-    EXPECT_TRUE(symbol.value() == 1);
+    EXPECT_EQ(symbol.value(), 1);
     EXPECT_TRUE(symbol.matches(1));
     EXPECT_FALSE(symbol.matches(0));
 
     symbol.setValue(0);
     EXPECT_FALSE(symbol.isDontCare());
-    EXPECT_TRUE(symbol.value() == 0);
+    EXPECT_EQ(symbol.value(), 0);
     EXPECT_TRUE(symbol.matches(0));
     EXPECT_FALSE(symbol.matches(1));
 
@@ -77,7 +77,7 @@ TEST(SymbolTest, SetValue)
 
     symbol.setValue(1);
     EXPECT_FALSE(symbol.isDontCare());
-    EXPECT_TRUE(symbol.value() == 1);
+    EXPECT_EQ(symbol.value(), 1);
     EXPECT_TRUE(symbol.matches(1));
     EXPECT_FALSE(symbol.matches(0));
 }
@@ -94,10 +94,10 @@ TEST(SymbolTest, AssignmentAndComparison)
     xcspp::Symbol one = symbol;
 
     symbol.setValue(0);
-    EXPECT_TRUE(symbol == zero);
-    EXPECT_TRUE(symbol != dontCare);
-    EXPECT_TRUE(symbol != one);
-    EXPECT_TRUE(zero != dontCare);
-    EXPECT_TRUE(zero != one);
-    EXPECT_TRUE(dontCare != one);
+    EXPECT_EQ(symbol, zero);
+    EXPECT_NE(symbol, dontCare);
+    EXPECT_NE(symbol, one);
+    EXPECT_NE(zero, dontCare);
+    EXPECT_NE(zero, one);
+    EXPECT_NE(dontCare, one);
 }
