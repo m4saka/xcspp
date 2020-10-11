ec84dc651a87b6880885981d380ca473e72134a3
masaka <masaka@mz-kb.com>
2020-10-12 05:30:10 +0900

    Fix symbol/action type in MOP header

--- include/xcspp/environment/majority_on_environment.hpp
+++ include/xcspp/environment/majority_on_environment.hpp
@@ -12,7 +12,7 @@ namespace xcspp
     {
     private:
         const std::size_t m_length;
-        std::vector<bool> m_situation;
+        std::vector<int> m_situation;
         bool m_isEndOfProblem;
         Random m_random;
 
@@ -24,10 +24,10 @@ namespace xcspp
         virtual ~MajorityOnEnvironment() = default;
 
         // Returns current situation
-        virtual std::vector<bool> situation() const override;
+        virtual std::vector<int> situation() const override;
 
         // Executes action (and update situation), and returns reward
-        virtual double executeAction(bool action) override;
+        virtual double executeAction(int action) override;
 
         // Returns true if the problem was solved by the previous action
         // (Since it is a single-step problem, this function always returns true after the first action execution)
@@ -40,7 +40,7 @@ namespace xcspp
         }
 
         // Returns answer to situation
-        bool getAnswer() const;
+        int getAnswer() const;
     };
 
 }
